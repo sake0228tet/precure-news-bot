9c679f0d98216aca0dc8fb3087c47fefef1f306b
sake0228tet <sake0228.tet@gmail.com>
2026-07-14 20:55:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
+CHANNEL_ID = 1523123948613795974
+
 intents = discord.Intents.default()
 
 client = discord.Client(intents=intents)
@@ -11,6 +13,15 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f"{client.user} が起動しました！")
+
+    channel = client.get_channel(CHANNEL_ID)
+
+    if channel:
+        await channel.send("🌸 プリキュアニュースBot 起動テスト！")
+    else:
+        print("チャンネルが見つかりませんでした")
+
+    await client.close()
 
 
 client.run(TOKEN)
