cacaf7a2f171917adfe029dc46b9d643ef31ffcd
sake0228tet <sake0228.tet@gmail.com>
2026-07-14 21:05:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import discord
 import os
+import feedparser
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+
+RSS_URL = "https://rss.app/feeds/1oU4mplx8CJl2VtR.xml"
 
 CHANNEL_ID = 1523123948613795974
 
@@ -14,12 +17,18 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print(f"{client.user} が起動しました！")
 
+    feed = feedparser.parse(RSS_URL)
+
+    latest = feed.entries[0]
+
+    print("タイトル:", latest.title)
+    print("URL:", latest.link)
+
     channel = client.get_channel(CHANNEL_ID)
 
-    if channel:
-        await channel.send("🌸 プリキュアニュースBot 起動テスト！")
-    else:
-        print("チャンネルが見つかりませんでした")
+    await channel.send(
+        f"🌸 RSSテスト\n{latest.title}\n{latest.link}"
+    )
 
     await client.close()
 
